6d999a45e5246e9c52643368c7fafa9385d87193
joseF <jose.farelo@hotmail.com>
2025-02-08 15:52:28 -0300

    feat(header, footer): finalizar componentes y agregar soporte responsivo
    - Añadidos archivos `footer.js` y `footer.scss`.
    - Implementados mixins para Header con medidas adaptadas a distintos dispositivos.
    - Incorporados mixins en Footer para asegurar un diseño responsivo en diferentes tamaños de pantalla.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -9,46 +9,53 @@ const Header = () => {
     return (
         <header className="header">
             <button
-                className="header-menu__button"
+                className="header__menu-button"
                 onClick={toggleMenu}
             >
                 {menuOpen ? (
                     <img 
                         src={require("../../assets/icons/menu-bar/menu-bar-icon-open.png")} 
                         alt="Abrir menú" 
+                        className="header__menu-icon"
                     />
                 ) : (
                     <img 
                         src={require("../../assets/icons/menu-bar/menu-bar-icon-close.png")} 
                         alt="Cerrar menú" 
+                        className="header__menu-icon"
                     />
                 )}
 
                 {/* Despliega el acceso rápido a otras secciones */}
-                <div className={`header__menu-open ${menuOpen ? 'header-menu--close' : 'header-menu--open'}`}>
-                    <div className="header__menu__container">
+                <nav className={`header__menu ${menuOpen ? 'header__menu--closed' : 'header__menu--open'}`}>
+                    <div className="header__menu-container">
                         <a href="/rutinas">Rutinas</a>
                     </div>
-                    <div className="header__menu__container">
+                    <div className="header__menu-container">
                         <a href="/alimentacion">Alimentación</a>
                     </div>
-                    <div className="header__menu__container">
+                    <div className="header__menu-container">
                         <a href="/progreso">Progreso</a>
                     </div>
-                    <div className="header__menu__container">
+                    <div className="header__menu-container">
                         <a href="/contacto">Contacto</a>
                     </div>
-                </div>
+                </nav>
             </button>
-
-            <a href="/" className="header__logo">
-                <svg 
-                    className="header__icon" 
-                    xmlns="http://www.w3.org/2000/svg">
-                    <image alt="text-icon" href={require("../../assets/icons/logo/logo-text/TF-logo-orange-text.png")} />
-                </svg>
-            </a>
-            <button className="header__button">INICIAR SESIÓN</button>
+            <div className= "header__logo-container">
+                <a href="/" className="header__logo">
+                    <svg 
+                        className="header__logo-icon" 
+                        xmlns="http://www.w3.org/2000/svg">
+                        <image 
+                            alt="text-icon" 
+                            href={require("../../assets/icons/logo/logo-text/TF-logo-orange-text.png")} 
+                            className="header__logo-image"
+                        />
+                    </svg>
+                </a>
+            </div>
+            <button className="header__login-button">INICIAR SESIÓN</button>
         </header>
     );
 };
