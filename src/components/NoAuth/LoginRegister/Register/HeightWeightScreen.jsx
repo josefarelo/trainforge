0bd8970b3cc221af90a03c201c386dafa1c7d396
joseF <jose.farelo@hotmail.com>
2025-04-15 13:15:25 -0300

    feat(HeightWeightScreen): Se creó el componente HeightWeightScreen con su respectivo diseño para cada resolución.     - Será fourth-screen de RegisterContent component.

---
+++ src/components/NoAuth/LoginRegister/Register/HeightWeightScreen.jsx
@@ -0,0 +1,123 @@
+import { useState } from "react";
+import './HeightWeightScreen.scss';
+import EmptyFields from "../../../EmptyFields";
+
+export default function HeightWeightScreen({ userData, updateUserData, onNext, onBack }) {
+
+    const [showError, setShowError] = useState(false);
+
+    const handleChange = (e) => {
+        const { name, value } = e.target
+        updateUserData({ [name]: value })
+    }
+    
+    const isMetric = userData.measurementSystem === "metric"
+
+    const isFormValid = () => {
+        if (isMetric) {
+            return userData.height_cm && userData.weight_kg
+        } else {
+            return userData.height_ft && userData.weight_lb
+        }
+    }
+
+    const handleNext = () => {
+        if (isFormValid()) {
+            setShowError(false);
+            onNext();
+        } else {
+            setShowError(true);
+        }
+    };
+    
+    return (
+        <div className="height-weight">
+
+            <div className="height-weight__content">
+                <h2 className="height-weight__title">Necesitaremos tu altura y peso. Servirán para el cálculo de tu TMB.</h2>
+
+                <div className="height-weight__measurement-system">
+                    <label className="height-weight__measurement-system-title">Sistema de medida</label>
+                    <select
+                        name="measurementSystem"
+                        value={userData.measurementSystem}
+                        onChange={handleChange}
+                        className="height-weight__measurement-system-input"
+                    >
+                        <option value="metric">Métrico (cm, kg)</option>
+                        <option value="imperial">Imperial (ft, lb)</option>
+                    </select>
+                </div>
+
+                <div className="height-weight__height-weight-content">
+                    {isMetric ? (
+                        <>
+                            <div className="height-weight__height">
+                                <label className="height-weight__height-title">Altura (cm)</label>
+                                <input
+                                    type="number"
+                                    name="height_cm"
+                                    value={userData.height_cm || ""}
+                                    onChange={handleChange}
+                                    className="height-weight__height-input"
+                                />
+                            </div>
+
+                            <div className="height-weight__weight">
+                                <label className="height-weight__weight-title">Peso (kg)</label>
+                                <input
+                                    type="number"
+                                    name="weight_kg"
+                                    value={userData.weight_kg || ""}
+                                    onChange={handleChange}
+                                    className="height-weight__weight-input"
+                                />
+                            </div>
+                        </>
+                    ) : (
+                        <>
+                            <div className="height-weight__height">
+                                <label className="height-weight__height-title">Altura (ft)</label>
+                                <input
+                                    type="number"
+                                    name="height_ft"
+                                    value={userData.height_ft || ""}
+                                    onChange={handleChange}
+                                    className="height-weight__height-input"
+                                />
+                            </div>
+
+                            <div className="height-weight__weight">
+                                <label className="height-weight__weight-title">Peso (lb)</label>
+                                <input
+                                    type="number"
+                                    name="weight_lb"
+                                    value={userData.weight_lb || ""}
+                                    onChange={handleChange}
+                                    className="height-weight__weight-input"
+                                />
+                            </div>
+                        </>
+                    )}
+                </div>
+
+                {showError && <EmptyFields className="height-weight__error"/>}
+            </div>
+
+            <div className="height-weight__button">
+                <button
+                    onClick={onBack}
+                    className="height-weight__button-back"
+                >
+                    Volver
+                </button>
+                <button
+                    onClick={handleNext}
+                    className="height-weight__button-next"
+                >
+                    Continuar
+                </button>
+            </div>
+        </div>
+    )
+}
